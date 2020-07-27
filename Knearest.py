91a8d50fe13022501383f261220d3c98a8ca77aa
jesse berkowitz <jberkow@brandeis.edu>
2020-07-27 13:18:52 -0400

    first model done

---
+++ Knearest.py
@@ -0,0 +1,64 @@
+class KNearestNeighbor:
+    def __init__(self, dataset, test_row, num_neighbors, row_others):
+        self.dataset = dataset 
+        self.test_row = test_row
+        self.num_neighbors = num_neighbors
+        self.row_others = row_others 
+        
+    #vector distance takes in a specific row, and finds the distance
+    # between that row and other rows, or in the case of a dataset, 
+    # all other rows in that dataset
+    # it scrolls through each value in the test_row and compares it to 
+    # values in the other specified rows
+    # it squares their distances, adds it to the distance value, and takes
+    # the square root of this value, to find the distance
+    def vector_distance(self, test_row, row_others):
+        distance = 0.0
+        for i in range(len(test_row)-1):
+            distance += (test_row[i] - row_others[i])**2
+        return distance**2
+    #fit takes in vector distance between specific rows and target row,
+    # adds them to a tuple, sorts the tuple by distances
+    # creates a new list called neighbors, and for specified number of neighbors,
+    # scrolls through the distances list, and appends the rows with the lowest
+    # vector_distance to the neighbors list, hence giving the nearest neighbors
+    # returns the neighbors list
+
+    def fit(self, dataset, test_row, num_neighbors):
+    
+        distances = list()
+        for row in dataset:
+            dist = self.vector_distance(test_row, row)
+            distances.append((row, dist))
+        distances.sort(key=lambda tup: tup[1])
+        neighbors = list()
+        for i in range(num_neighbors):
+            neighbors.append(distances[i][0])
+        return neighbors
+
+    # based on the neighbors list from the fit method,
+    # the predict method takes the last column in the neighbors rows, 
+    # which should most likely be the classification 0/1 value,
+    # takes the highest count of the nearest neighbors to the test row,
+    # and returns a prediction value, so if there are 10 1s, and 9 0s, it will
+    # predict a value of 1
+    def predict(self,dataset, test_row, num_neighbors):
+        neighbors = self.fit(dataset, test_row, num_neighbors)
+        output = [row[-1] for row in neighbors]
+        prediction = max(set(output), key=output.count)
+        return prediction
+
+dataset1 = [[2.7810836,2.550537003,0],
+	[1.465489372,2.362125076,1],
+	[3.396561688,4.400293529,0],
+	[1.38807019,1.850220317,1],
+	[3.06407232,3.005305973,0],
+	[7.627531214,2.759262235,1],
+	[5.332441248,2.088626775,1],
+	[6.922596716,1.77106367,1],
+	[8.675418651,-0.242068655,1],
+	[7.673756466,3.508563011,1]]
+
+prediction = KNearestNeighbor(dataset1, dataset1[0], 3, dataset1 )
+
+print(prediction.predict(dataset1, dataset1[0], 3))
--- model.py
+++ model.py
@@ -1,13 +1,7 @@
 from math import sqrt
-class KNearest:
-    def __init__(self, model):
-        self.model = model
-
-
-
 
 #calculate the Euclidean distance between two vectors
-def euclidean_distance(row_x, row2):
+def vector_distance(row_x, row2):
 	distance = 0.0
 	for i in range(len(row_x)-1):
 		distance += (row_x[i] - row2[i])**2
@@ -35,7 +29,7 @@ dataset = [[2.7810836,2.550537003,0],
 row0 = dataset[0]
 #row0 is the specified row you are looking to take difference in distance of
 for row in dataset:
-	distance = euclidean_distance(row0, row)
+	distance = vector_distance(row0, row)
 # row represents scrolling through all other rows in the dataset
 # to compare to row0
 
@@ -47,7 +41,7 @@ def get_neighbors(dataset, test_row, num_neighbors):
 	distances = list()
 	for row in dataset:
 
-		dist = euclidean_distance(test_row, row)
+		dist = vector_distance(test_row, row)
 		distances.append((row, dist))
 	distances.sort(key=lambda tup: tup[1])
 	neighbors = list()
@@ -68,3 +62,53 @@ close_neighbors = get_neighbors(dataset, dataset[0], 3)
 
 for neighbor in close_neighbors:
 	print(neighbor)
+#So this function is first finding the closest neighbors to the specific row being
+# tested...then it is finding the classification column, which in this case,
+# is the last column , represented by row[-1] for each closely related row in neighbors
+# then it is finding the most common value through this max(set) function, 
+# and the majority value in this column will be returned as a prediction
+def predict(dataset, test_row, num_neighbors):
+	neighbors = get_neighbors(dataset, test_row, num_neighbors)
+	output_values = [row[-1] for row in neighbors]
+	prediction = max(set(output_values), key=output_values.count)
+	return prediction
+#the function below is using the above prediction with the same dataset, 
+# and for the given prediction, using dataset, column 0, last row, which is the 
+# test row's classification number as expected, and "got" represents the 
+# prediction from predict
+prediction = predict(dataset, dataset[0], 3)
+print('Expected %d, Got %d.' % (dataset[0][-1], prediction))
+
+class KNearestNeighbor:
+    def __init__(self, dataset):
+		self.dataset = dataset 
+		
+
+	def vector_distance(row_x, row2):
+		distance = 0.0
+	for i in range(len(row_x)-1):
+		distance += (row_x[i] - row2[i])**2
+	return sqrt(distance)
+
+
+	def fit(dataset, test_row, num_neighbors):
+    
+	distances = list()
+	for row in dataset:
+
+		dist = vector_distance(test_row, row)
+		distances.append((row, dist))
+	distances.sort(key=lambda tup: tup[1])
+	neighbors = list()
+	for i in range(num_neighbors):
+		neighbors.append(distances[i][0])
+	return neighbors
+
+
+	def predict(dataset, test_row, num_neighbors):
+	neighbors = fit(dataset, test_row, num_neighbors)
+	output = [row[-1] for row in neighbors]
+	prediction = max(set(output), key=output.count)
+	return prediction
+
+

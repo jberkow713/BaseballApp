27d968776a50822ff6595c3e5a8791350e8e43b1
jesse berkowitz <jberkow@brandeis.edu>
2020-07-31 09:29:37 -0400

    new

--- knearest2.py
+++ knearest2.py
@@ -50,20 +50,63 @@ class KNearestNeighbor:
 				correct += 1
 		return correct / float(len(output)) * 100.0  
 
+
+from csv import reader
+# reading in a csv file, into necessary dataset
+
+def load_dataset(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
+# this is turning a string into a float
+# so that it can be evaluated in the str_column_to_int method
+
+
+def string_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+
+
+#youre scrolling through a dataset, row by row, and finding a specific column, 
+# in the case of the iris dataset, it is the last column, specified by (len(dataset[0])-1)
+# youre taking all specific values in that column, throughout the dataset, and you are storing them in an array,
+# Distinct represents the different values in values array
+# taking the set values from Distinct, adding them to Dictionary
+# you are then making the value in the specific row, equal to the new enumerated Dictionary value
+# Hence manually encoding the classification variable
+
+
+
+def string_to_int(dataset, column):
+	class_values = [row[column] for row in dataset]
+	unique = set(class_values)
+	lookup = dict()
+	for i, value in enumerate(unique):
+		lookup[value] = i
+		print('[%s] => %d' % (value, i))
+	for row in dataset:
+		row[column] = lookup[row[column]]
+	return lookup
 dataset = load_dataset('iris.csv')	
 for i in range(len(dataset[0])-1):
 	string_to_float(dataset, i)
 string_to_int(dataset, len(dataset[0])-1)
-
 prediction = KNearestNeighbor(dataset)
 
-row = [1,.3,1.5,1.5]
 
-x3 = prediction.fit(dataset, row, 9)
-print(x3)
 
-x4 = prediction.predict(dataset, row, 9, x3)
+row = [8,1,0.2,3.4]
 
-x5 = prediction.conclusiveness(dataset, row, 9, x3, x4)
-print(x4)
-print(x5)        
+x1 = prediction.fit(dataset, row, 9)
+print(x1)
+
+x2 = prediction.predict(dataset, row, 9, x1)
+
+x3 = prediction.conclusiveness(dataset, row, 9, x1, x2)
+print(x2)
+print(x3)        
